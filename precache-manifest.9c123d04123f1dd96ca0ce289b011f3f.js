95d3b6ccbe0dd89ccc652e6e61e166ecbf06898a
bstanfield <bnstnfld@gmail.com>
2020-02-16 18:09:58 -0800

    Updates

---
+++ precache-manifest.9c123d04123f1dd96ca0ce289b011f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cb6d287cb799963d80d010c4c3a0a3",
+    "url": "/test-catchwords/index.html"
+  },
+  {
+    "revision": "dfd64824293f83ca6d7e",
+    "url": "/test-catchwords/static/js/2.5fdc6266.chunk.js"
+  },
+  {
+    "revision": "2aa3285f84d34c7922a8",
+    "url": "/test-catchwords/static/js/main.0ef656ff.chunk.js"
+  },
+  {
+    "revision": "867cff79a7e392c2dd7c",
+    "url": "/test-catchwords/static/js/runtime-main.57cb257c.js"
+  },
+  {
+    "revision": "d73c42bd888769c31504619a5f8dc3f9",
+    "url": "/test-catchwords/static/media/alert.d73c42bd.svg"
+  },
+  {
+    "revision": "fab2dd997752f77ee29cc21f9c326d1a",
+    "url": "/test-catchwords/static/media/checkmark.fab2dd99.svg"
+  }
+]);
